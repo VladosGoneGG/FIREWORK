af736e022e2d53fd0867b234320a7e9d83440f6c
Vladislav Medelian <vlad93gm@gmail.com>
2026-09-09 22:22:40 +0300

    Make the sidebar IP name link to the requisites download
    Restores the requisites .doc as a download, triggered by clicking
    "ИП Федяков Иван Владимирович" in the sidebar.

--- src/components/PromoPanel/PromoPanel.jsx
+++ src/components/PromoPanel/PromoPanel.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const PromoPanel = () => {
 	return (
-		<div className='bg-white p-5   mt-[20px] w-[240px] h-[235px]  rounded-[20px] shadow-[0_0_10px_0_rgba(0,0,0,0.2)]'>
+		<div className='bg-white p-5   mt-[20px] w-[240px]   rounded-[20px] shadow-[0_0_10px_0_rgba(0,0,0,0.2)]'>
 			<div className='flex flex-col   text-[12px] font-bold text-[#625a51] font-baron  tracking-wider'>
 				<ul className=' flex flex-col gap-1 '>
 					<li className='hover:text-[#bd52e9] active:text-[#997DF5] cursor-pointer self-start '>
@@ -15,12 +15,19 @@ const PromoPanel = () => {
 
 				<ul className=' flex flex-col leading-tight '>
 					<li className='my-[20px] hover:text-[#bd52e9] active:text-[#997DF5] cursor-pointer self-start'>
-						<a href='/documents/litsenziya.pdf' target='_blank' rel='noopener noreferrer' download>
+						<a
+							href='/documents/litsenziya.pdf'
+							target='_blank'
+							rel='noopener noreferrer'
+							download
+						>
 							сертификат профессионального пиротехника
 						</a>
 					</li>
-					<li>
-						ип федяков иван <br /> владимирович
+					<li className='hover:text-[#bd52e9] active:text-[#997DF5] cursor-pointer self-start'>
+						<a href='/documents/rekvizity.doc' download>
+							ип федяков иван <br /> владимирович
+						</a>
 					</li>
 				</ul>
 			</div>
